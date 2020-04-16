f5296fd919f0fad8e53e7ba072c2602b620461d7
Gabrielle Simon <gabriellesimon@Gabrielles-MacBook-Pro.local>
2020-04-16 14:39:54 -0500

    Map 2 Final

--- map2/map2.js
+++ map2/map2.js
@@ -1,7 +1,13 @@
-var map2 = L.map('map2').setView([31.07, -92.40], 5)
-L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png').addTo(map2)
+var map2 = L.map('map2').setView([31.07, -92.40], 6)
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map2)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(map2)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
